087e5acf5cdcb7cd827325b7cc5acdbae091601e
Grover Light <grover.light@gmail.com>
2016-11-04 09:14:48 -0700

    Initial Commit

---
+++ captionProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  captionProject
+//
+//  Created by Grover Light on 11/4/16.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
