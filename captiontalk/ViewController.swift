10b8b7bbf5621863f19e59fcaca1e9cfb6c95e60
Aaron Liu <aaronliu@Aarons-MacBook-Air.local>
2016-11-05 11:11:08 -0700

    checkpoint

--- captiontalk/ViewController.swift
+++ captiontalk/ViewController.swift
@@ -9,11 +9,12 @@
 import UIKit
 import AVFoundation
 import QuartzCore
-class ViewController: UIViewController,AVCaptureFileOutputRecordingDelegate{
+import Speech
+class ViewController: UIViewController,AVCaptureFileOutputRecordingDelegate, SFSpeechRecognizerDelegate{
     var captureSession: AVCaptureSession!
     var previewLayer = AVCaptureVideoPreviewLayer()
     var deleteVideoHelper: FileManager? = FileManager()
-    
+    var speechString = String()
     @IBOutlet weak var progressBarBack: UIView!
     @IBOutlet weak var progressBar: UIView!
     @IBOutlet weak var bigLabel: UILabel!
@@ -31,7 +32,12 @@ class ViewController: UIViewController,AVCaptureFileOutputRecordingDelegate{
         return NSURL(string: tempPath!)!
     }()
 
-
+    //speech private vars
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US"))
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -39,6 +45,7 @@ class ViewController: UIViewController,AVCaptureFileOutputRecordingDelegate{
         //add hold and release gestures to uibutton
         fatButton.addTarget(self, action: #selector(holdDown(sender:)), for: UIControlEvents.touchDown)
         fatButton.addTarget(self, action: #selector(holdRelease(sender:)), for: UIControlEvents.touchUpInside)
+        fatButton.addTarget(self, action: #selector(holdRelease(sender:)), for: UIControlEvents.touchDragExit)
         // hide progressbar
         progressBar.isHidden = true
         progressBarBack.isHidden = true
@@ -72,13 +79,36 @@ class ViewController: UIViewController,AVCaptureFileOutputRecordingDelegate{
         captureSession.addOutput(videoFileOutput)
         captureSession.startRunning()
         
-        
-       
-        
-
-
-
-        
+        // set up speech
+      
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in  //4
+            
+           
+            var isButtonEnabled = true
+            switch authStatus {  //5
+            case .authorized:
+                isButtonEnabled = true
+                
+            case .denied:
+                isButtonEnabled = false
+                print("User denied access to speech recognition")
+                
+            case .restricted:
+                isButtonEnabled = false
+                print("Speech recognition restricted on this device")
+                
+            case .notDetermined:
+                isButtonEnabled = false
+                print("Speech recognition not yet authorized")
+            }
+            
+            if (isButtonEnabled == true){
+                self.fatButton.isEnabled = true
+            }
+            else{
+                self.fatButton.isEnabled = false
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -104,25 +134,31 @@ class ViewController: UIViewController,AVCaptureFileOutputRecordingDelegate{
 
     func holdDown(sender:UIButton){
         let recordingDelegate:AVCaptureFileOutputRecordingDelegate? = self
-        
-        
-      //  let filePath = NSURL.fileURL(withPath: NSTemporaryDirectory(),isDirectory: true)
-        print ("hold down")
+        if (self.smallLabel.isHidden == false){
+            self.smallLabel.isHidden = true
+        }
         if (progressBar.isHidden == true){
             progressBar.isHidden = false
             progressBarBack.isHidden = false
             
-            UIView.animate(withDuration: 5) { () -> Void in
+
+            UIView.animate(withDuration: 5, animations: { 
                 self.progressBar.transform = CGAffineTransform(scaleX: 0.000001, y: 1)
-            }
+            }, completion: { (completion) in
+                if (self.videoFileOutput.isRecording == true){
+                    self.videoFileOutput.stopRecording()
+                }
+            })
         }
         videoFileOutput.startRecording(toOutputFileURL: tempFilePath as URL!, recordingDelegate: recordingDelegate)
-        
+        recordSpeech()
         
     }
     
     func holdRelease(sender:UIButton){
-        print ("release")
+        if (self.smallLabel.isHidden == true){
+            self.smallLabel.isHidden = false
+        }
         videoFileOutput.stopRecording()
         if (progressBar.isHidden == false){
             progressBar.isHidden = true
@@ -130,19 +166,96 @@ class ViewController: UIViewController,AVCaptureFileOutputRecordingDelegate{
             progressBar.layer.removeAllAnimations()
             progressBar.transform = CGAffineTransform(scaleX: 1, y: 1)
             }
+        if (audioEngine.isRunning == true){
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+        }
         
     }
     
 
- 
+    func recordSpeech() -> Void {
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryRecord)
+            try audioSession.setMode(AVAudioSessionModeMeasurement)
+            try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+        } catch {
+            print("audioSession properties weren't set because of an error.")
+        }
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        guard let inputNode = audioEngine.inputNode else {
+            fatalError("Audio engine has no input node")
+        }
+        
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+        }
+        
+        recognitionRequest.shouldReportPartialResults = true
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+            
+            var isFinal = false
+            
+            if result != nil {
+                self.speechString = (result?.bestTranscription.formattedString)!
+                print(self.speechString)
+                isFinal = (result?.isFinal)!
+            }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.fatButton.isEnabled = true
+            }
+        })
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        
+        do {
+            try audioEngine.start()
+        } catch {
+            print("audioEngine couldn't start because of an error.")
+        }
+        
+    }
+
+
+    //AVCaptureFileOutputRecordingDelegate delegate methods
     func capture(_ captureOutput: AVCaptureFileOutput!, didStartRecordingToOutputFileAt fileURL: URL!, fromConnections connections: [Any]!) {
-        print("recording start start")
+        print("recording...")
     }
     
     
     func capture(_ captureOutput: AVCaptureFileOutput!, didFinishRecordingToOutputFileAt outputFileURL: URL!, fromConnections connections: [Any]!, error: Error!) {
-        print ("recording finished")
-        print(outputFileURL)
+        print ("DONE!")
+    }
+    
+    
+    // speechrecognizer delegate
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            fatButton.isEnabled = true
+        } else {
+            fatButton.isEnabled = false
+        }
     }
     
     
